d7d34f2145f43bba588b6d840f93626a18e61a09
Blake Scala <blakescala@gmail.com>
2017-04-23 22:05:28 -0400

    add form that saves user input as variable and displays

---
+++ factorial.php
@@ -0,0 +1,15 @@
+<?php
+  $starting_number = $_GET["starting_number"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>Your starting number is <?php echo $starting_number?></h3>
+  </body>
+</html>
