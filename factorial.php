dc10b2e4008a68695a197bad07741ada05af9304
Blake Scala <blakescala@gmail.com>
2017-04-25 20:10:10 -0400

    add repeater form

--- factorial.php
+++ factorial.php
@@ -1,6 +1,13 @@
 <?php
   $starting_number = $_GET["starting_number"];
 
+  $factorial = function factorial($starting_number) {
+        if ($starting_number < 2) {
+            return 1;
+        } else {
+            return ($starting_number * factorial($starting_number-1));
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +17,6 @@
     <title></title>
   </head>
   <body>
-    <h3>Your starting number is <?php echo $starting_number?></h3>
+    <h3>The factorial of <?php echo $starting_number?> is <?php factorial($starting_number) ?></h3>
   </body>
 </html>
